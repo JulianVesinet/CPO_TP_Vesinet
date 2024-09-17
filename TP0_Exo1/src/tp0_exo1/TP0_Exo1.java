e70715c489399572821d2efdcd2fb8d94ab0701b
Julian Vesinet <julian.vesinet@epfedu.fr>
2024-09-17 17:03:16 +0200

    Création du projet exo 1 : affichage d'un message

---
+++ TP0_Exo1/src/tp0_exo1/TP0_Exo1.java
@@ -0,0 +1,29 @@
+/*
+* TP0 Exercie 1
+* TD C-Groupe 1
+* VESINET Julian
+* 17 Septembre 2024
+*/
+
+package tp0_exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ninon
+ */
+public class TP0_Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// TODO code application logic here
+         Scanner sc;
+ sc = new Scanner(System.in);
+ System.out.println("Bonjour, quel jour est-il ?");
+ sc.nextLine();
+    }
+   
+}
