7115dea1409aeb8fbb9d7e300337749c711485a8
Julian Vesinet <julian.vesinet@epfedu.fr>
2024-11-13 11:45:43 +0100

    TP 2 Exercice 4

---
+++ TP1_stats_VESINET/src/tp1_stats_vesinet/Random.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp1_stats_vesinet;
+
+/**
+ *
+ * @author julia
+ */
+class Random {
+
+    int nestInt(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    int nextInt(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
